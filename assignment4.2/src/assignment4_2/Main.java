f50258bf67502cb48c5202108a8822d3e91a798d
Barış ERTUĞRUL <barisertugrul@ertyazilim.com>
2021-05-04 01:10:59 +0300

    Update Main.java

--- assignment4.2/src/assignment4_2/Main.java
+++ assignment4.2/src/assignment4_2/Main.java
@@ -3,7 +3,9 @@ package assignment4_2;
 import java.time.LocalDate;
 
 import assignment4_2.Abstract.BaseCustomerManager;
+import assignment4_2.Adapters.CustomerCheckManager;
 import assignment4_2.Adapters.MernisServiceAdapter;
+import assignment4_2.Adapters.TCKNValidate;
 import assignment4_2.Concrete.NeroCustomerManager;
 import assignment4_2.Concrete.StarbucksCustomerManager;
 import assignment4_2.Entities.Customer;
@@ -12,15 +14,39 @@ public class Main {
 
 	public static void main(String[] args) {
         
-        Customer starbucksCustomer = new Customer(
+        Customer starbucksCustomer1 = new Customer(
         		"Barýþ",
         		"Ertuðrul",
         		LocalDate.of(1977, 01, 01),
         		"12345678910"
         		);
         
+
+        Customer starbucksCustomer2 = new Customer(
+        		"Beril",
+        		"Özsoy",
+        		LocalDate.of(1980, 01, 01),
+        		"12345678911"
+        		);
+
+        Customer starbucksCustomer3 = new Customer(
+        		"Engin",
+        		"Demiroð",
+        		LocalDate.of(1985, 01, 01),
+        		"12345678912"
+        		);
+        
+		//Gerçek TCKN Kontrollü Kayýt
 		BaseCustomerManager starbucksCustomerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
-		starbucksCustomerManager.save(starbucksCustomer);
+		starbucksCustomerManager.save(starbucksCustomer1);
+		
+		//Sadece TCKN Algoritma Kontrollü Kayýt
+		starbucksCustomerManager = new StarbucksCustomerManager(new TCKNValidate());
+		starbucksCustomerManager.save(starbucksCustomer2);
+
+		//Fake Doðrulamalý Kayýt
+		starbucksCustomerManager = new StarbucksCustomerManager(new CustomerCheckManager());
+		starbucksCustomerManager.save(starbucksCustomer3);
 		
 
         
@@ -28,7 +54,7 @@ public class Main {
         		"Çaðan Ege",
         		"Ertuðrul",
         		LocalDate.of(2007, 01, 01),
-        		"12345678911"
+        		"12345678913"
         		);
         
 		BaseCustomerManager neroCustomerManager = new NeroCustomerManager();
